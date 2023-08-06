d35e52204b329770ab441a835ede7fc24a79b969
kgnamba <kelseynamba@gmail.com>
2023-08-06 00:45:06 -0700

    Only using necessary fields

--- api/app/graphql/mutations/edit_chatroom.rb
+++ api/app/graphql/mutations/edit_chatroom.rb
@@ -1,17 +1,14 @@
 module Mutations
     class EditChatroom < BaseMutation
       argument :chatroom_id, ID, required: true
-      argument :label, String, required: true
-      argument :caller_phone_number, String, required: true
       argument :description, String, required: false
-      argument :nature_code_id, ID, required: false
   
       # fields
       field :chatroom, Types::ChatroomType, null: false
   
       # resolver
-      def resolve(chatroom_id:, label:, caller_phone_number:, description: nil, nature_code_id: nil )
-        params = { label:, caller_phone_number:, description:, nature_code_id: }.compact_blank
+      def resolve(chatroom_id:, description: nil )
+        params = { description: }.compact_blank
         chatroom = Chatroom.update(chatroom_id, **params)
         
         {
