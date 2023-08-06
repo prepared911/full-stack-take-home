8dc33c66494d8cfcb0cfb895c71701bf058a2733
kgnamba <kelseynamba@gmail.com>
2023-08-06 00:32:52 -0700

    added edit chatroom mutation

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_chatroom, mutation: Mutations::CreateChatroom
+    field :edit_chatroom, mutation: Mutations::EditChatroom
   end
 end
